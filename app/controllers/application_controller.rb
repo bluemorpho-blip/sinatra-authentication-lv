785ea92701e09d2ed5d8ad5bf889a4c79699db67
Ronald Stephens <rstephens@bluemorpho.cc>
2019-11-06 02:03:03 +0000

    form in login.erb

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,14 +7,5 @@ class ApplicationController < Sinatra::Base
     set :session_secret, "auth_demo_lv"
   end
 
-  get '/' do
-    session[:greeting] = "Hello World"
-    response.set_cookie 'credit_amount', '100'
-    "Hello World!"
-  end
-
-  get '/remember' do
-    "AYou have #{request.cookies[credit_amount]} left to spend"
-  end
 
 end
--- config.ru
+++ config.ru
@@ -1,5 +1,5 @@
 require './config/environment'
 
 use Rack::Session::Cookie
-
+use SessionsController
 run ApplicationController
