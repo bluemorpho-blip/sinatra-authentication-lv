6072a12266201d2a6d7196f4cc1ee6468661c092
Ronald Stephens <rstephens@bluemorpho.cc>
2019-11-06 01:41:02 +0000

    cookies

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,12 +3,18 @@ class ApplicationController < Sinatra::Base
   configure do
     set :public_folder, 'public'
     set :views, 'app/views'
-    # enable :sessions
-    # set :session_secret, "carcollection"
+    enable :sessions
+    set :session_secret, "auth_demo_lv"
   end
 
   get '/' do
+    session[:greeting] = "Hello World"
+    response.set_cookie 'credit_amount', '100'
     "Hello World!"
+  end
+
+  get '/remember' do
+    "AYou have #{request.cookies[credit_amount]} left to spend"
   end
 
 end
--- config.ru
+++ config.ru
@@ -1,3 +1,5 @@
 require './config/environment'
 
+use Rack::Session::Cookie
+
 run ApplicationController
